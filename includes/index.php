8cafba59108eda484b8c6e175ba2cdcc808961c9
fotomxq <fotomxq@163.com>
2016-04-19 10:23:31 +0800

    清空php数据,重新以beego建立项目.

--- includes/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * 空文件
- * @author liuzilu <fotomxq@gmail.com>
- * @version 1
- * @package core
- */
-?>
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ ":/Go/src/ftmp-stock/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
