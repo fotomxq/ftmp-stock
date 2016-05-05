5148721d189bd05ae6cd74a2d7bceaf5568616b6
fotomxq <fotomxq@163.com>
2016-05-05 17:41:54 +0800

    继续完善基础框架

--- models/file_operate.go
+++ models/file_operate.go
@@ -1,8 +1,16 @@
 //文件操作封装
 package models
 
-func init() {
+import (
+	"os"
+)
 
+func init() {
+}
+
+//打开一个文件
+func OpenFile(src string) (*File, error) {
+	return os.Open(src)
 }
 
 //创建文件
@@ -25,6 +33,42 @@ func WriteFile(src string, content string) (bool, error) {
 	return false, nil
 }
 
+//写入文件，末端插入数据
 func WriteFileAppend(src string, content string) (bool, error) {
 	return false, nil
+}
+
+//修改文件名称
+func EditFileName(src string, newName string) (bool, error) {
+	return false, nil
+}
+
+//删除文件或文件夹
+func DeleteFile(src string) (bool, error) {
+	return false, nil
+}
+
+//判断是否为文件
+func IsFile(src string) (bool, error) {
+	return false, nil
+}
+
+//判断是否为文件夹
+func IsFolder(src string) (bool, error) {
+	return false, nil
+}
+
+//获取子文件列表
+func GetFileList(src string) ([]string, error) {
+	return nil, nil
+}
+
+//获取文件的大小
+func GetFileSize(src string) (int, error) {
+	return 0, nil
+}
+
+//获取文件夹的大小
+func GetFolderSize(src string) (int, error) {
+	return 0, nil
 }
