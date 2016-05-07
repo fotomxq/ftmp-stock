ab85f39839085614009c31f258cb494ce409ed40
fotomxq <fotomxq@163.com>
2016-05-07 08:31:59 +0800

    no message

--- models/file_operate.go
+++ models/file_operate.go
@@ -2,73 +2,85 @@
 package models
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func init() {
-}
-
-//打开一个文件
-func OpenFile(src string) (*File, error) {
-	return os.Open(src)
-}
-
-//创建文件
-func CreateFile(src string, content string) (bool, error) {
-	return false, nil
+	src := "c:/c.txt"
+	fmt.Println(ReadFile(src))
 }
 
 //创建目录
-func CreateDir(src string, name string) (bool, error) {
-	return false, nil
+func CreateDir(src string, name string) bool {
+	return false
 }
 
 //读取文件
-func ReadFile(src string) (string, error) {
-	return "", nil
+func ReadFile(src string) string {
+	file, err := os.Open(src)
+	if err != nil {
+		panic(err)
+		return ""
+	}
+	defer file.Close()
+	c, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+		return ""
+	}
+	return string(c)
 }
 
 //写入文件
-func WriteFile(src string, content string) (bool, error) {
-	return false, nil
-}
-
-//写入文件，末端插入数据
-func WriteFileAppend(src string, content string) (bool, error) {
-	return false, nil
+func WriteFile(src string, content string) bool {
+	read := strings.NewReader(content)
+	newC, errNewC := read.ReadByte()
+	if errNewC != nil {
+		panic(errNewC)
+		return false
+	}
+	errWriteFile := ioutil.WriteFile(src, newC)
+	if errWriteFile != nil {
+		panic(errWriteFile)
+		return false
+	}
+	return true
 }
 
 //修改文件名称
-func EditFileName(src string, newName string) (bool, error) {
-	return false, nil
+func EditFileName(src string, newName string) bool {
+	return false
 }
 
 //删除文件或文件夹
-func DeleteFile(src string) (bool, error) {
-	return false, nil
+func DeleteFile(src string) bool {
+	return false
 }
 
 //判断是否为文件
-func IsFile(src string) (bool, error) {
-	return false, nil
+func IsFile(src string) bool {
+	return false
 }
 
 //判断是否为文件夹
-func IsFolder(src string) (bool, error) {
-	return false, nil
+func IsFolder(src string) bool {
+	return false
 }
 
 //获取子文件列表
-func GetFileList(src string) ([]string, error) {
-	return nil, nil
+func GetFileList(src string) string {
+	return ""
 }
 
 //获取文件的大小
-func GetFileSize(src string) (int, error) {
-	return 0, nil
+func GetFileSize(src string) int {
+	return 0
 }
 
 //获取文件夹的大小
-func GetFolderSize(src string) (int, error) {
-	return 0, nil
+func GetFolderSize(src string) int {
+	return 0
 }
